0a58747494a3c8346884d625bdda52da5b439b64
MamaevVladimir <ipmamaev.v@gmail.com>
2019-07-09 11:15:02 +0300

    внесены изменения

--- src/lesson21/Main.java
+++ src/lesson21/Main.java
@@ -2,7 +2,7 @@ package lesson21;
 
 public class Main {
     //private static final Logger. Logger апач в списке отсутствует
-    public static void main(String[] args) {
+    public static void main(String[] args) {// ожидается полный видеоматериал по уроку
 
     }
 }
